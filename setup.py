b76d1c05346d72132a51d956d276359b81d7f858
Paul Tremberth <paul.tremberth@gmail.com>
2017-01-20 15:29:12 +0100

    Add Python 3.6 classifier

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=['scrapy', 'jsonschema', 'six']
 )
