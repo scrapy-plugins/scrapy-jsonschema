e34e3e5230183691ae74cca87118fa7d66f250a2
VMruiz <victormruizs@gmail.com>
2019-05-30 16:11:59 +0200

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scrapy-jsonschema',
-    version='0.2.0',
+    version='0.2.1',
     license='BSD',
     description='Scrapy schema validation pipeline and Item builder using JSON Schema',
     author='Scrapinghub',
