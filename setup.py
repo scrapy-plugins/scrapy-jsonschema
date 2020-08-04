f1c6d5056c26170fa23e5e517426f99920a3d4c4
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2020-08-04 14:28:03 +0800

    add support for Python 3.8

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     install_requires=['scrapy', 'jsonschema[format]', 'six']
 )
